9e346cfe7fe232667380d29ae88767d56eb244b7
Justin Forsyth <justinforsyth@macbook-air.local>
2015-10-12 21:52:16 -0400

    Initial Commit

---
+++ Easing/Easing.h
@@ -0,0 +1,19 @@
+//
+//  Easing.h
+//  Easing
+//
+//  Created by Justin Forsyth on 10/12/15.
+//  Copyright © 2015 jforce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Easing.
+FOUNDATION_EXPORT double EasingVersionNumber;
+
+//! Project version string for Easing.
+FOUNDATION_EXPORT const unsigned char EasingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Easing/PublicHeader.h>
+
+
